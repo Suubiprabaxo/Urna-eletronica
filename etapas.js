e01f23bc2d25dbd3f4ff93f7f25dd6b690c6a934
Lucas França <102264849+Suubiprabaxo@users.noreply.github.com>
2022-08-30 19:36:44 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,33 @@
+let etapas = [
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '22',
+                nome: 'THUTHUCA DO CENTRÃO',
+                partido: 'DOS QUE ODEIA COMUNISMO TAOKEI',
+                fotos: [
+                    { url: 'bolso.jpg', legenda: 'PRESIDENTE', small: false }
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'LADRÃO É POUCO',
+                partido: 'DO EX ARREPENDIDO QUE TE BATEU E AGORA VAI SER TUDO DIFERENTE',
+                fotos: [
+                    { url: 'lula.jpg', legenda: 'PRESIDENTE' }
+                ]
+            },
+            {
+                numero: '12',
+                nome: 'CIRANDA CIRANDINHA',
+                partido: 'DOR NA COLUNA DE CARREGAR OS PROJETOS DO BRASIL',
+                fotos: [
+                    { url: 'ciro.jpeg', legenda: 'PRESIDENTE' }
+                ]
+            },
+        ]
+    },
+    
+];
